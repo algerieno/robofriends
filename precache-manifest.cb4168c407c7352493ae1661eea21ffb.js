f00f9f47a3802701e539f1fde8cdf5c20eac7b00
algerieno <aymen.algerieno2014@gmail.com>
2020-07-17 12:33:44 +0100

    Updates

---
+++ precache-manifest.cb4168c407c7352493ae1661eea21ffb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ef7cb8a0efd6a62536686b2df1212c1",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "93cbf165e2919896834b",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "df79b0cce1d7d2bcf706",
+    "url": "/robofriends/static/css/main.d723d1f1.chunk.css"
+  },
+  {
+    "revision": "93cbf165e2919896834b",
+    "url": "/robofriends/static/js/2.342815a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.342815a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df79b0cce1d7d2bcf706",
+    "url": "/robofriends/static/js/main.01c0723c.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/robofriends/static/media/logo.5d5d9eef.svg"
+  }
+]);
